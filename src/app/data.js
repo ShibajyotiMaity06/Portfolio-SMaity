9939b6bcb4910f372e1323a8b33aa2b80199e482
deba-maity <debajyoti.maity29@gmail.com>
2024-05-18 09:48:38 +0530

    first

--- src/app/data.js
+++ src/app/data.js
@@ -1,91 +1,33 @@
-/*
-Websites:
 
-- https://github.com/pmndrs/gltfjsx (GLTF JSX for 3D Models)
-- https://lucide.dev/icons/ (Lucide Icons)
-- https://github.com/anuraghazra/github-readme-stats (Github Readme Stats)
-- https://skillicons.dev (Skill Icons to show skills)
-- https://github-readme-streak-stats.herokuapp.com (Github Readme Streak Stats)
-
-:root {
-  --background: 27 27 27;
-  --foreground: 225 225 225;
-  --muted: 115 115 115;
-  --accent: 254 254 91; #FEFE5B
-}
-
-*/
 
 export const projectsData = [
   {
     id: 1,
-    name: "EcoTracker",
-    description: "Track your carbon footprint",
-    date: "2022-08-15",
-    demoLink: "https://ecotracker.example.com",
+    name: "Real Estate Website",
+    description: "Created a dynamic real estate frontend website where several properties can be showcased for Business purposes.",
+    date: "2024-02",
+    demoLink: "https://realestate-ayc.pages.dev/",
   },
   {
     id: 2,
-    name: "ArtGallery Online",
-    description: "Digital art showcase platform",
-    date: "2022-06-20",
-    demoLink: "https://artgalleryonline.example.com",
+    name: "YOOOMMM",
+    description: "Developed an App with features like authentication and call scheduling, recording, personal meeting room.",
+    date: "2024-03",
+    demoLink: "https://yoooommm.vercel.app/",
   },
   {
     id: 3,
-    name: "BudgetPlanner",
-    description: "Plan and track expenses",
-    date: "2022-09-10",
-    demoLink: "https://budgetplanner.example.com",
+    name: "AI-BOT",
+    description: "Created an automated AI-Speech recognition based Universal search machine.",
+    date: "2023-10",
+    demoLink: "https://github.com/ShibajyotiMaity06/JARVIS-AI-BOT-",
   },
   {
     id: 4,
-    name: "HealthBeat",
-    description: "Monitor heart rate zones",
-    date: "2022-05-30",
-    demoLink: "https://healthbeat.example.com",
-  },
-  {
-    id: 5,
-    name: "RecipeFinder",
-    description: "Discover new recipes",
-    date: "2022-07-12",
-    demoLink: "https://recipefinder.example.com",
-  },
-  {
-    id: 6,
-    name: "JourneyLogger",
-    description: "Log your travels",
-    date: "2022-10-01",
-    demoLink: "https://journeylogger.example.com",
-  },
-  {
-    id: 7,
-    name: "StudyBuddy",
-    description: "Collaborative learning platform",
-    date: "2022-04-18",
-    demoLink: "https://studybuddy.example.com",
-  },
-  {
-    id: 8,
-    name: "TechTalk",
-    description: "Tech news aggregator",
-    date: "2022-11-05",
-    demoLink: "https://techtalk.example.com",
-  },
-  {
-    id: 9,
-    name: "FitTrack",
-    description: "Fitness and workout tracker",
-    date: "2022-03-22",
-    demoLink: "https://fittrack.example.com",
-  },
-  {
-    id: 10,
-    name: "MindfulMoments",
-    description: "Meditation and mindfulness app",
-    date: "2022-02-14",
-    demoLink: "https://mindfulmoments.example.com",
+    name: "Alarm System",
+    description: "Developed an alarm system to trigger alert upon detecting motion.",
+    date: "2022-12",
+    demoLink: "https://github.com/ShibajyotiMaity06/alarmsecurity",
   },
 ];
 
@@ -96,25 +38,19 @@ export const BtnList = [
   { label: "Contact", link: "/contact", icon: "contact", newTab: false },
   {
     label: "Github",
-    link: "https://www.github.com/codebucks27",
+    link: "https://github.com/ShibajyotiMaity06",
     icon: "github",
     newTab: true,
   },
   {
     label: "LinkedIn",
-    link: "https://www.linkedin.com/in/codebucks",
+    link: "https://www.linkedin.com/in/shibajyoti-maity-5992b126a/",
     icon: "linkedin",
     newTab: true,
   },
   {
-    label: "X",
-    link: "https://www.x.com/code_bucks",
-    icon: "twitter",
-    newTab: true,
-  },
-  {
     label: "Resume",
-    link: "/resume.pdf",
+    link: "/Shibajyoti_Maity_Resume.pdf",
     icon: "resume",
     newTab: true,
   },
---
+++ src/components/about/index.jsx
@@ -0,0 +1,131 @@
+import React from "react";
+import ItemLayout from "./ItemLayout";
+
+const AboutDetails = () => {
+  const iconStyle = {
+    width: "48px",
+    height: "48px",
+    position: "relative",
+    display: "inline-block",
+    overflow: "hidden",
+    borderRadius: "12px",
+  };
+
+  const imgStyle = {
+    width: "100%",
+    height: "100%",
+    borderRadius: "12px",
+  };
+
+  const pseudoElementStyle = {
+    position: "absolute",
+    bottom: 0,
+    left: 0,
+    width: "100%",
+    height: "100%",
+    borderRadius: "12px",
+    background:
+      "linear-gradient(to top, rgba(255, 255, 255, 0.3), rgba(255, 255, 255, 0))",
+    borderBottom: "2px solid rgba(255, 255, 255, 0.3)",
+    borderTop: "2px solid transparent",
+    transition: "background 0.3s ease, border 0.3s ease",
+    pointerEvents: "none",
+  };
+
+  const linkStyle = {
+    position: "relative",
+    display: "inline-block",
+    width: "48px",
+    height: "48px",
+  };
+
+  const links = [
+    {
+      href: "https://github.com/ShibajyotiMaity06",
+      imgSrc: "https://skillicons.dev/icons?i=github",
+      alt: "GitHub",
+    },
+    {
+      href: "https://linkedin.com/in/shibajyoti-maity-5992b126a/",
+      imgSrc: "https://skillicons.dev/icons?i=linkedin",
+      alt: "LinkedIn",
+    },
+    {
+      href: "https://codeforces.com/profile/shibajyoti.maity06",
+      imgSrc:
+        "https://store-images.s-microsoft.com/image/apps.48094.14504742535903781.aedbca21-113a-48f4-b001-4204e73b22fc.503f883f-8339-4dc5-8609-81713a59281f",
+      alt: "Codeforces",
+    },
+    {
+      href: "https://leetcode.com/u/maity_0602_/",
+      imgSrc:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQyjQXt2Qiqaa-jSNO2xNbv8xw7n-euVm1Itap-oY-Bpg&s",
+      alt: "LeetCode",
+    },
+    {
+      href: "https://wa.me/+918074088423",
+      imgSrc: "https://cdn-icons-png.flaticon.com/512/3670/3670051.png",
+      alt: "WhatsApp",
+    },
+  ];
+
+  return (
+    <section className="py-20 w-full">
+      <div className="grid grid-cols-12 gap-4 xs:gap-6 md:gap-8 w-full">
+        <ItemLayout className="col-span-full lg:col-span-8 row-span-2 flex-col items-start">
+          <h2 className="text-xl md:text-2xl text-left w-full capitalize">
+            Architect of Enchantment
+          </h2>
+          <p className="font-light text-xs sm:text-sm md:text-base">
+            Currently pursuing my B.Tech in Computer Science from Dr. Lankapalli
+            Bullayya College of Engineering, I specialize in web development
+            with JavaScript at the core. Proficient in React.js and Next.js, I
+            create seamless, visually captivating websites using Jamstack for
+            speed and security. As a machine learning engineer, I leverage
+            Python for intelligent solutions and predictive insights. Active on
+            Codeforces, I continually hone my coding skills. Looking ahead, I
+            plan to expand my expertise into Web3 and cloud computing to further
+            innovate in the digital realm. Join me on this journey of
+            technological exploration and innovation.
+          </p>
+        </ItemLayout>
+
+        <ItemLayout className="col-span-full">
+          <img
+            className="w-full h-auto"
+            src={`https://skillicons.dev/icons?i=appwrite,aws,css,figma,firebase,git,github,graphql,html,js,jquery,mongodb,mysql,netlify,nextjs,nodejs,npm,react,redux,replit,sass,tailwind,threejs,vercel,vite,vscode,yarn`}
+            alt="CodeBucks"
+            loading="lazy"
+          />
+        </ItemLayout>
+
+        <ItemLayout className="col-span-full lg:col-span-8 row-span-2 flex-col items-start">
+          <h2 className="text-xl md:text-2xl text-left w-full capitalize">
+            Socially I'm here
+          </h2>
+          <div className="flex space-x-4 mt-4">
+            {links.map((link) => (
+              <a
+                key={link.href}
+                href={link.href}
+                target="_blank"
+                rel="noopener noreferrer"
+                style={linkStyle}
+              >
+                <img
+                  src={link.imgSrc}
+                  alt={link.alt}
+                  style={imgStyle}
+                  className="hover:opacity-80"
+                />
+                <div style={pseudoElementStyle}></div>
+              </a>
+            ))}
+          </div>
+        </ItemLayout>
+      </div>
+    </section>
+  );
+};
+
+export default AboutDetails;
