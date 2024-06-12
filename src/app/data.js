6d26290337cde55beb4c25505d7e65df4be7eb1a
deba-maity <debajyoti.maity29@gmail.com>
2024-06-12 12:00:46 +0530

    new changes on projects

--- src/app/data.js
+++ src/app/data.js
@@ -3,32 +3,54 @@
 export const projectsData = [
   {
     id: 1,
+    name: "Self driving Car",
+    description: "Built and trained a CNN model to drive a car automatically.",
+    date: "2024-06",
+    demoLink: "https://github.com/ShibajyotiMaity06/self-driving-car-model/blob/main/self_driving_car.ipynb",
+  },
+  {
+    id: 2,
+    name: "Emotion Detection",
+    description: " Built and trained RNN model for key facial detection points and emotion detection",
+    date: "2024-05",
+    demoLink: "https://github.com/ShibajyotiMaity06/Emotion_detection/blob/main/maity_Emotion_AI.ipynb",
+  },
+  {
+    id: 3,
+    name: "Tumor Detection",
+    description: "Built and trained a Segmentation ResUnet Model to localize Brain Tumors.",
+    date: "2024-05",
+    demoLink: "https://github.com/ShibajyotiMaity06/tumour_detection/blob/main/maity_Healthcare_AI.ipynb",
+  },
+  {
+    id: 4,
     name: "Real Estate Website",
     description: "Created a dynamic real estate frontend website where several properties can be showcased for Business purposes.",
     date: "2024-02",
     demoLink: "https://realestate-ayc.pages.dev/",
   },
   {
-    id: 2,
+    id: 5,
     name: "YOOOMMM",
     description: "Developed an App with features like authentication and call scheduling, recording, personal meeting room.",
     date: "2024-03",
     demoLink: "https://yoooommm.vercel.app/",
   },
   {
-    id: 3,
+    id: 6,
     name: "AI-BOT",
     description: "Created an automated AI-Speech recognition based Universal search machine.",
     date: "2023-10",
     demoLink: "https://github.com/ShibajyotiMaity06/JARVIS-AI-BOT-",
   },
   {
-    id: 4,
+    id: 7,
     name: "Alarm System",
     description: "Developed an alarm system to trigger alert upon detecting motion.",
     date: "2022-12",
     demoLink: "https://github.com/ShibajyotiMaity06/alarmsecurity",
   },
+  
 ];
 
 export const BtnList = [
